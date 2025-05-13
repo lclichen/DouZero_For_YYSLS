30cdca05b7c51b0b046b954e95b78c104872b91f
lclichen <mar@mail.ustc.edu.cn>
2025-05-13 15:27:23 +0800

    UPDATE

--- test_white_check.py
+++ test_white_check.py
@@ -14,14 +14,15 @@ def have_white(img):  # 是否有白块
             pass
     return False
 def check_pass(img):
-    try:
-        result = pyautogui.locateAll(needleImage='pics_yysls/pass.png', haystackImage=img, grayscale=False, confidence=0.8)
-        list(result)
-        return True
-    except Exception as e:
-        print(e)
-        return False
+    for pass_img in ["pass-qi", "pass-qi-dark"]:
+        try:
+            result = pyautogui.locateAll(needleImage=f'pics_yysls/{pass_img}.png', haystackImage=img, grayscale=False, confidence=0.8)
+            list(result)
+            return True
+        except Exception as e:
+            print(e)
+    return False
 for i in range(256):
-    img_path = f"./logs/20250512@210221/white_check_{i}.png"
+    img_path = f"./logs/20250513@151714/white_check_{i}.png"
     img_i = Image.open(img_path)
     print(i, have_white(img_i), check_pass(img_i))
